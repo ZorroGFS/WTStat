7e25909a5d6e5386be96804d8919f782f687432e
ZorroGFS <github@zorrogfs.info>
2018-08-31 13:30:03 +0300

    save-load, national symbols strip, top padding

--- WTStat.py
+++ WTStat.py
@@ -11,39 +11,45 @@ ip_address  = ""
 user_name   = ""
 lastEvt = 0
 lastDmg = 0
+maxLen = 0
 stats = {}
-statk = {}
-statd = {}
 shows = {}
 obsData = ""
 trying = False
 doParse = False
+debug = False
 
 # ------------------------------------------------------------
 
+def cleanname(name):
+	global maxLen
+#	(ger)(isr)(ussr)(usa)(uk)(jp)
+	res = name.strip('▀\uf059▂▃▄▅')
+	if len(res) > maxLen:
+		maxLen = len(res)
+	return res
+
 def addkill(key):
 	global stats
-	global statk
-	global statd
+	key = cleanname(key)
 	stats['kills'] += 1
-	if key in statk:
-		statk[key] += 1
+	if key in stats['k']:
+		stats['k'][key] += 1
 	else:
-		statk[key] = 1
-	if key not in statd:
-		statd[key] = 0
+		stats['k'][key] = 1
+	if key not in stats['d']:
+		stats['d'][key] = 0
 
 def adddeath(key):
 	global stats
-	global statk
-	global statd
+	key = cleanname(key)
 	stats['deaths'] += 1
-	if key in statd:
-		statd[key] +=1
+	if key in stats['d']:
+		stats['d'][key] +=1
 	else:
-		statd[key] = 1
-	if key not in statk:
-		statk[key] = 0
+		stats['d'][key] = 1
+	if key not in stats['k']:
+		stats['k'][key] = 0
 
 def addburn():
 	global stats
@@ -66,6 +72,7 @@ def parse_damage(msg):
 			addkill(m.group(2))
 		if m.group(3) == user_name:
 			adddeath(m.group(4))
+		log("Rule 1")
 		return
 	m = re.match("([^(]+) \((.+)\) уничтожил ([^(]+) \((.+)\)", msg)
 	if m:
@@ -73,62 +80,77 @@ def parse_damage(msg):
 			addkill(m.group(2))
 		if m.group(3) == user_name:
 			adddeath(m.group(4))
+		log("Rule 2")
 		return
 	m = re.match("([^(]+) \((.+)\) разбился", msg)
 	if m:
 		if m.group(1) == user_name:
 			adddeath(m.group(2))
+		log("Rule 3")
 		return
 	m = re.match("([^(]+) \((.+)\) выведен из строя", msg)
 	if m:
 		if m.group(1) == user_name:
 			adddeath(m.group(2))
+		log("Rule 4")
 		return
 	m = re.match("([^(]+) \((.+)\) получил \"(.+)\"", msg)
 	if m:
 		if m.group(1) == user_name:
 			addmedal()
+		log("Rule 5")
 		return
 	m = re.match("([^(]+) \((.+)\) подбил ([^(]+) \((.+)\)", msg)
 	if m:
 		if m.group(1) == user_name:
 			addcrit()
+		log("Rule 6")
 		return
 	m = re.match("([^(]+) \((.+)\) поджёг ([^(]+) \((.+)\)", msg)
 	if m:
 		if m.group(1) == user_name:
 			addburn()
+		log("Rule 7")
 		return
 	m = re.match("([^(]+) \((.+)\) нанёс последний удар!", msg)
 	if m:
+		log("Rule 8")
 		return
 	m = re.match("([^(]+) \((.+)\) ударил первым!", msg)
 	if m:
+		log("Rule 9")
 		return
 	m = re.match("(.+) присоединился к событию", msg)
 	if m:
+		log("Rule 10")
 		return
 	m = re.match("(.+) сбил ([^(]+) \((.+)\)", msg)
 	if m:
 		if m.group(2) == user_name:
 			adddeath(m.group(3))
+		log("Rule 11")
 		return
 	m = re.match("(.+) уничтожил ([^(]+) \((.+)\)", msg)
 	if m:
 		if m.group(2) == user_name:
 			adddeath(m.group(3))
+		log("Rule 12")
 		return
 	m = re.match("(.+) сбил (.+)", msg)
 	if m:
+		log("Rule 13")
 		return
 	m = re.match("(.+) уничтожил (.+)", msg)
 	if m:
+		log("Rule 14")
 		return
 	m = re.match("(.+) подбил (.+)", msg)
 	if m:
+		log("Rule 15")
 		return
 	m = re.match("(.+) поджёг (.+)", msg)
 	if m:
+		log("Rule 16")
 		return
 	log("Not parsed: " + msg)
 
@@ -149,23 +171,23 @@ def make_text():
 	return res.strip()
 
 def make_top():
-	global statk
-	global statd
+	global stats
+	global maxLen
 	res = ""
 	i = 0
-	for key in sorted(statk, key=statk.__getitem__, reverse=True):
+	for key in sorted(stats['k'], key=stats['k'].__getitem__, reverse=True):
 		if i == 5:
 			break
 		else:
 			i += 1
-		res += key + " " + str(statk[key]) + "/" + str(statd[key]) + "\n"
+		res += key.ljust(maxLen) + " " + str(stats['k'][key]) + "/" + str(stats['d'][key]) + "\n"
 	return res.strip()
 
 def log(msg):
-	f = open(os.path.realpath(__file__)+"_battle.log","a", encoding="utf-8")
-	try:
-		f.write(str(msg)+"\n")
-	finally:
+	global debug
+	if debug:
+		with open(os.path.realpath(__file__)+"_battle.log","a", encoding="utf-8") as f:
+			f.write(str(msg)+"\n")
 		f.close()
 	
 def update_text():
@@ -190,7 +212,7 @@ def update_text():
 				for evt in js['events']:
 					lastEvt = evt['id']
 				for dmg in js['damage']:
-#					log(dmg)
+					log(dmg)
 					lastDmg = dmg['id']
 					if doParse:
 						parse_damage(dmg['msg'])
@@ -216,17 +238,32 @@ def update_text():
 
 def reset_stat():
 	global stats
-	global statk
-	global statd
 	stats={}
-	statd={}
-	statk={}
+	stats['k']={}
+	stats['d']={}
 	stats['kills'] = 0
 	stats['burns'] = 0
 	stats['deaths'] = 0	
 	stats['medals'] = 0
 	stats['crits'] = 0
-	
+
+def save_stat():
+	global stats
+	with open(os.path.realpath(__file__)+"_save.dat","w") as f:
+		json.dump(stats, f)
+	f.close()
+
+def load_stat():
+	global stats
+	global maxLen
+	with open(os.path.realpath(__file__)+"_save.dat","r") as f:
+		stats = json.load(f)
+	f.close()
+	maxLen = 0
+	for k in stats['k'].keys():
+		if len(k) > maxLen:
+			maxLen = len(k)
+
 # --- Buttons
 	
 def refresh_pressed(props, prop):
@@ -240,12 +277,13 @@ def refresh_pressed(props, prop):
 	script_update(obsData)
 
 def statreset_pressed(props, prop):
-#	global stats
-#	global obsData
 	reset_stat()
-#	stats['kills'] = obs.obs_data_get_int(obsData, "ikills")
-#	stats['burns'] = obs.obs_data_get_int(obsData, "iburns")
-#	stats['deaths'] = obs.obs_data_get_int(obsData, "ideaths")
+
+def save_pressed(props, prop):
+	save_stat()
+
+def load_pressed(props, prop):
+	load_stat()
 
 # --- OBS exports
 	
@@ -253,6 +291,10 @@ def script_load(settings):
 	global obsData
 	obsData = settings
 	reset_stat()
+	load_stat()
+
+def script_unload():
+	save_stat()
 
 def script_defaults(settings):
 	obs.obs_data_set_default_string(settings, "ip_address", "127.0.0.1:8111")
@@ -263,7 +305,7 @@ def script_defaults(settings):
 	obs.obs_data_set_default_bool(settings, "smedals", False)
 	
 def script_description():
-	return "WT Писькомер v. 0.003\n(cc) ZorroGFS"
+	return "WT Писькомер v. 0.004\n(cc) ZorroGFS"
 
 def script_update(settings):
 	global source_name
@@ -315,5 +357,7 @@ def script_properties():
 	obs.obs_properties_add_bool(props, "scrits", "Криты")
 	obs.obs_properties_add_bool(props, "smedals", "Награды")
 	obs.obs_properties_add_button(props, "button1", "Сброс статистики", statreset_pressed)
+	obs.obs_properties_add_button(props, "button_save", "Сохранить статистику", save_pressed)
+	obs.obs_properties_add_button(props, "button_load", "Загрузить статистику", load_pressed)
 	obs.obs_properties_add_button(props, "button2", "Переподключиться к WT", refresh_pressed)
 	return props
